a10464638385b3202c77f50624fe5f4bf4c8df98
xujj921228 <280926174@qq.com>
2019-10-17 17:08:31 +0800

    change something

--- RLS/Sources/main.c
+++ RLS/Sources/main.c
@@ -8,39 +8,32 @@
 #include "lin_diagnostic_service.h"
 #include "clock.h"
 #include "lin.h"
-#include "adc.h"
 #include "gpio.h"
 #include "eeprom.h"
 #include "ftm.h"
-#include "spi.h"
-#include "pmc.h"
 #include "rtc.h"
 #include "watchdog.h"
-#include "rls_app.h"
 #include "lin_app.h"
 #include "humid.h"
-#include "iic.h"
 
-/***********************************************************************************************
-*
-* @brief    main - Initialize 
-* @param    none
-* @return   none
-*
-************************************************************************************************/  
 #define VERIFIED_SECTOR				32
-#define BUS_CLK_HZ					24000000
-
+/***********************************************************************************************
+* name :  main
+* input:  none
+* output: none
+* introduction:For lin-boot
+* author:xujunjie@bb.com
+*2019/10/17
+************************************************************************************************/  
 uint8  u8DataBuff[256];
 uint8  u8DataBuff_read[512];
 void main(void)
 {	
 	char ch;
 	uint32 i;
-	Clk_Init();	
-	FTM0_Init();
-	GPIO_Init();//Setting the LDO of LIN to control Power.   
-	RTC_Init();
+	Clk_Init();	  //time for MCU;
+	FTM0_Init();  //TIM
+	GPIO_Init();  //Setting the LDO of LIN to control Power.   
 	//Lin_Sys_Init();
 	/*Initialize the Flash Memory module */
 	    FLASH_Init(BUS_CLCOK);
@@ -66,18 +59,14 @@ void main(void)
 	        for(ch =0;ch<16;ch++)
 	        {
 	        	u8DataBuff_read[i*16+ch] = *((uint8_t *)(i*16+ch+VERIFIED_SECTOR*FLASH_SECTOR_SIZE));
-	            //printf("0x%x,",*((uint8_t *)(i*16+ch+VERIFIED_SECTOR*FLASH_SECTOR_SIZE)));
 	        }
-	       // printf("\n\r");
 	    }
 
-	    //printf("All data has been read.\n\r");
 	
 	
 	for(;;) 
 	{	
-		WDOG_Feed();
+		 WDOG_Feed();	
 		
-				
-	 }	
+	}	
 }
